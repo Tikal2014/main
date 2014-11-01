4b881a8d06fed9d22ea65937fbac9c102fe77694
andrewbartlett <andrewba@buffalo.edu>
2014-11-01 12:21:22 -0400

    Update Player.java
    added comments

--- Player.java
+++ Player.java
@@ -5,28 +5,28 @@ public class Player {
 	private static int AP = 8;
 	private static String _currentPlayer = "Player1";
 
-	// If we assume AP isn't carried over, don't need to worry about which
+	// If we assume AP isn't carried over each turn, don't need to worry about which
 	// player has which AP
 	public static void reduceAP() {
-		AP--;
+		AP--; //decrement AP
 	}
 
 	public static int getAP() {
-		return AP;
+		return AP; 
 	}
 
 	public void resetAP() {
-		AP = 8;
+		AP = 8; //at end of turn, sets AP back to 8 for next player
 	}
 
 	public static boolean enoughAP(){
-		return AP>0;
+		return AP>0; //check if AP>0, only 1 AP required for any action
 	}
 	public static void switchPlayer() {
 		if (_currentPlayer == "Player1") {
 			_currentPlayer = "Player2";
 		} else if (_currentPlayer == "Player2") {
-			_currentPlayer = "Player1";
+			_currentPlayer = "Player1"; //changes from p1 to p2
 		}
 	}
 
