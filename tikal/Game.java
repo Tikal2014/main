67af237575baf46ecdc620c104c05f87d7a502f2
andrewbartlett <andrewba@buffalo.edu>
2014-11-05 16:53:07 -0500

    Update Game.java
    minor fixes

--- tikal/Game.java
+++ tikal/Game.java
@@ -7,9 +7,9 @@ public class Game {
 	 */
 /*	private int _tileCount = 0;
 	Tile[] tilePositions = new Tile[33]; //array of tiles, with indices as positions
-	Integer[] p1ExplorerPositions = new Integer[33]; //array of player 1 explorer counts and positions
-	Integer[] p2ExplorerPositions = new Integer[33]; //same for p2
-	Integer[] pyramidPositions = new Integer[33]; //array of pyramid positions and values
+	int[] p1ExplorerPositions = new int[33]; //array of player 1 explorer counts and positions
+	int[] p2ExplorerPositions = new int[33]; //same for p2
+	int[] pyramidPositions = new int[33]; //array of pyramid positions and values
 */
 	private board;
 	
@@ -17,7 +17,7 @@ public class Game {
 		board = new Board;
 	}
 	
-	public void placeExplorer(int tile) { //placing an explorer, at the tile position used as a parameter
+	public int[] placeExplorer(int tile) { //placing an explorer, at the tile position used as a parameter
 		if (Player.enoughAP()) {//checks for enough AP
 			if (Player.getPlayer() == "Player1") {
 				p1ExplorerPositions[tile]++; //place a p1 explorer
@@ -27,15 +27,22 @@ public class Game {
 				Player.reduceAP();
 			}
 		}
+	if (Player.getPlayer() == "Player1") {
+		return p1ExplorerPositions;
+        	}
+        else{
+            return p2ExplorerPositions;
+        	}
 	}
 
-	public void placePyramid(int tile) {
+	public int[] placePyramid(int tile) {
 		if (Player.enoughAP()) {
 			if (Tile.hasPyramid(tile)) { //check if tiles has at least a base pyramid
 				pyramidPositions[tile]++; //add pyramid level
 				Player.reduceAP();
 			}
 		}
+		return pyramidPositions;
 	}
 
 	public void moveExplorer(int startTile, int endTile) { //moving from start tile (current) to end tile
@@ -75,10 +82,10 @@ public class Game {
 
 	}
 
-	private int scoreP1(Integer[] ep1, Integer[] ep2, Integer[] pp) {
+	private int scoreP1(int[] ep1, int[] ep2, int[] pp) {
 		int p1Score = 0;
 		for (int i = 0; i < 33; i++) {
-			if (pp[i] != null) {
+			if (pp[i] != 0) {
 				if (ep1[i] > ep2[i]) { // 1 point for every pyramid level on spaces where p1 has more explorers than p2
 					p1Score = p1Score + pp[i];
 				}
@@ -95,10 +102,10 @@ public class Game {
 		return p1Score;
 	}
 
-	private int scoreP2(Integer[] ep1, Integer[] ep2, Integer[] pp) {
+	private int scoreP2(int[] ep1, int[] ep2, int[] pp) {
 		int p2Score = 0;
 		for (int i = 0; i < 33; i++) {
-			if (pp[i] != null) {
+			if (pp[i] != 0) {
 				if (ep1[i] < ep2[i]) {
 					p2Score = p2Score + pp[i];
 				}
