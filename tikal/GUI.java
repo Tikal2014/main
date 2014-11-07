dcbd1e67dad535095e3c2687d414af3b754888db
Kevin Duggan <kevindug@buffalo.edu>
2014-11-07 13:21:03 -0500

    Clean up messy files, get everything in right file

--- tikal/GUI.java
+++ tikal/GUI.java
@@ -1,20 +1,8 @@
 package tikal;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.util.ArrayList;
-
-import javax.swing.AbstractButton;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 //***************************************************
 // I use layeredPane to utilize the graph part of our project.
 // For example, the background lies in the bottom layer, the 1st layer is the tiles, and the top layer is buytton.s
@@ -27,9 +15,7 @@ import javax.swing.JPanel;
 
 
 
-
 import tikal.ButtonListener;
-
 
 public class GUI {			// I define everything here
 	
@@ -38,33 +24,32 @@ public class GUI {			// I define everything here
      JLayeredPane lpane = new JLayeredPane();
      JPanel backPanel=new JPanel();
      JPanel panel2=new JPanel();
-     JPanel panel3=new JPanel();  // this JPanel will be the control pad place
-     JButton _exButt = new JButton("ADD EXPLORER");
-     JButton _endTurnButt = new JButton("END TURN");
-     int _player=1; // set default turn is player #1
 
-     ArrayList<ArrayList<JPanel>> jpButt=new ArrayList<ArrayList<JPanel>>();
-     ArrayList<ArrayList<JPanel>> jpTile=new ArrayList<ArrayList<JPanel>>();
+     ArrayList<ArrayList<JPanel>> jpButt = new ArrayList<ArrayList<JPanel>>();
+     ArrayList<ArrayList<JPanel>> jpTile = new ArrayList<ArrayList<JPanel>>();
      
      
     public GUI() //this is the constructor
     		//I define GUI in a simplier way compare to our previous work, there is no run() and we can directly 
     		// create the oject(a window popup) by running GUI. (It works through last method you see in this file)
     {
-    	for(int i=0;i<6;i++){
-    		
-    		jpButt.add(new ArrayList<JPanel>());
+	    	for(int i=0;i<6;i++){
+	    		
+	    		jpButt.add(new ArrayList<JPanel>());
 			jpTile.add(new ArrayList<JPanel>());
-    		for(int j=0;j<6;j++){	
-    			jpButt.get(i).add(new JPanel());
-    			jpTile.get(i).add(new JPanel()); 	
-    			jpButt.get(i).get(j).setLayout(new BorderLayout());
-    		}
-
-    	}
+			
+	    		for(int j=0;j<6;j++){	
+	    			jpButt.get(i).add(new JPanel());
+	    			jpTile.get(i).add(new JPanel()); 			
+	    		}
+	    		
+	    	}
     	
-    	JLabel aa = new JLabel();   
-    	ImageIcon im = new ImageIcon("images/tikalmap1.jpg"); //background
+	    	JLabel aa = new JLabel();   
+	    	ImageIcon im = new ImageIcon("images/tikalmap1.jpg"); //background
+			
+		
+		
 		
         frame.setPreferredSize(new Dimension(1280, 768)); // create frame size
         frame.setLayout(new BorderLayout());   // using borderlayout, basically I just want to put everyhing in the middle
@@ -75,106 +60,87 @@ public class GUI {			// I define everything here
         
         //***************iamgeIcons set Icon(image)
         aa.setIcon(im);
+        
 
         //*************** add imageIcons to Jpanels
+        
         backPanel.add(aa);
+ 
         //*************** Jpenels' settings
         backPanel.setBounds(0, 0, 1024, 768);
         backPanel.setOpaque(true);
         		
-        //************** Contro pad Jpanel
-        panel3.setBounds(1000, 300, 300, 300);
-        panel3.setOpaque(true);
-        _exButt.addActionListener(new ButtonListener(this,1,1,_g)); //maybe need a separate listener
-        _endTurnButt.addActionListener(new ButtonListener(this,1,1,_g)); //maybe need a separate listener
-        panel3.add(_exButt);
-        panel3.add(_endTurnButt);
         //*************** add jpanels to the container
         tileRefresh();
         buttonRefresh();
-        lpane.add(backPanel, new Integer(0), 0);    
-        lpane.add(panel3, new Integer(5), 0); 
+        lpane.add(backPanel, new Integer(0), 0);      
         frame.pack();
         frame.setVisible(true);
     }
-    public void tileRefresh() { 
-    	
-       
+    
+    public void tileRefresh() {       
 		for (int i = 0; i < 6; i++) { // add tile
 			for (int j = 0; j < 6; j++){
-			ImageIcon tt = new ImageIcon("images/rect2.png");
-			JLabel tempLabel = new JLabel();
-			tempLabel.setIcon(tt);
-			jpTile.get(i).get(j).add(tempLabel);
-			lpane.add(jpTile.get(i).get(j), new Integer(1), 0);
-			jpTile.get(i).get(j).setBackground(new Color(0,0,0,0));
-			//***************** set different positon for odd and even
-			if(i%2==0){ //even
-			jpTile.get(i).get(j).setBounds(200+i*125,92+92*j, 160,160);
+				ImageIcon tt = new ImageIcon("images/rect2.png");
+				JLabel tempLabel = new JLabel();
+				tempLabel.setIcon(tt);
+				jpTile.get(i).get(j).add(tempLabel);
+				lpane.add(jpTile.get(i).get(j), new Integer(1), 0);
+				jpTile.get(i).get(j).setBackground(new Color(0,0,0,0));
+				//***************** set different positon for odd and even
+				if(i%2==0){
+				jpTile.get(i).get(j).setBounds(200+i*125,92+92*j, 120,135);
+				}
+				else{
+				jpTile.get(i).get(j).setBounds(200+i*125,50+92*j, 120,135);
+				}
+				
+				//***************** odd&even end
+				
+				jpTile.get(i).get(j).setVisible(false);
 			}
-			else{
-			jpTile.get(i).get(j).setBounds(200+i*125,50+92*j, 160,160);
-			}
-			
-			//***************** odd&even end
-			
-			jpTile.get(i).get(j).setVisible(true);
-			explorerAdd(i,j,1);
-			jpTile.get(0).get(5).setVisible(false);// to make 5,6,5,6,5,6 game board
-			jpTile.get(2).get(5).setVisible(false);
-			jpTile.get(4).get(5).setVisible(false);
-		}
 		}
 	}
     
 	public void buttonRefresh() {
     	
+		JButton[] b = new JButton[36];
+		int n = 0;
+			
 		for (int i = 0; i < 6; i++) { // add button
 			for (int j = 0; j < 6; j++){
-			String temp="x:"+i+" y:"+j;
-			JButton b = new JButton(temp);
-			b.addActionListener(new ButtonListener(this,i,j,_g));
-			jpButt.get(i).get(j).add(b, BorderLayout.CENTER);
-			lpane.add(jpButt.get(i).get(j), new Integer(2), 0);
-			jpButt.get(i).get(j).setBackground(new Color(0,0,0,0));
-			
-
-			//***************** set different positon for odd and even
-			if(i%2==0){//even
-			jpButt.get(i).get(j).setBounds(235+i*125,102+92*j, 80,40);
-			
+				String temp="x:"+i+" y:"+j;
+				b[n] = new JButton(temp);
+				jpButt.get(i).get(j).add(b[n]);
+				lpane.add(jpButt.get(i).get(j), new Integer(2), 0);
+				jpButt.get(i).get(j).setBackground(new Color(0,0,0,0));
+				//***************** set different positon for odd and even
+				if(i%2==0){
+					jpButt.get(i).get(j).setBounds(200+i*125,102+92*j, 160,40);
+				}
+				else{
+					jpButt.get(i).get(j).setBounds(200+i*125,60+92*j, 160,40);
+				}
+				
+				b[n].addActionListener(new ButtonListener(n, jpTile));
+				n++;
 			}
-			else{
-			jpButt.get(i).get(j).setBounds(235+i*125,60+92*j, 80,40);
-			}
-			
-			//***************** odd&even end
-//			jpButt.get(i).get(j).setBounds(200+i*125,60+92*j, 160,40);
-			}
-			jpButt.get(0).get(5).setVisible(false);// to make 5,6,5,6,5,6 game board
-			jpButt.get(2).get(5).setVisible(false);
-			jpButt.get(4).get(5).setVisible(false);
 		}
-		
 	}
 
 	public void setButtonText(int i, int j, String arg0){   //reset the text in button
 		JButton b = new JButton(arg0);
 		jpButt.get(i).get(j).removeAll();
-		jpButt.get(i).get(j).add(b, BorderLayout.CENTER);
+		jpButt.get(i).get(j).add(b);
 	}
 	
-	public void addTile(int i,int j,int player){
-		_player=player;  //*** for later use
-		jpTile.get(i).get(j).setVisible(true);
+	public void setTileOnBoard(int i, int j){
+		
+		
 		
 	}
-	public void explorerAdd(int i, int j, int player){
-		
-		JLabel jlabel = new JLabel("Jerk#1");
-	    jlabel.setFont(new Font("Verdana",1,10));
-		jpButt.get(i).get(j).add(jlabel, BorderLayout.NORTH);
-	}
+	
+	
     /**
      * @param args the command line arguments
      */
--- tikal/README.md
+++ tikal/README.md
@@ -1,4 +1,25 @@
 main
 ====
 
-Code for the Tikal game
+CSE 116 Project Phase 1 - 11/7/14
+
+Group Members:
+Yangyong Zhang (yangyong@buffalo.edu)
+Andrew Bartlett (andrewba@buffalo.edu)
+Shuai Yan (shuaiyan2@live.com)
+Yuchuan Cheng (yuchuanc@buffalo.edu)
+Kevin Duggan (kevindug@buffalo.edu)
+
+Phase 1 Tikal Functionality:
+-Data structures which keep track of tile positions, explorer positions, and pyramid positions, in addition to explorer quantities and pyramid levels
+-GUI with ability to display individual tiles
+-Tiles which present the number of explorers for each player and pyramid level they contain
+-Defined methods for player actions: place explorer, place pyramid, move explorer, and end turn
+-Player class which tracks remaining AP and current player
+-Player class has methods which check if AP>0, reduce AP, reset AP, and switch current player
+-Scoring methods which, when the game ends (the last tile is placed) calculate and return the score of each player
+
+Planned Phase 2 Functionality:
+-Player interface that allows players to perform actions and present them with game information
+-Ability to determine tile placement legality based on edge paths
+
